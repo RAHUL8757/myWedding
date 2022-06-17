d8328dea30122d2dd4579e9f2d79a26c3284291d
rahulsingh19 <rahul.libra1928@gmail.com>
2022-06-17 09:11:45 +0530

    add photo on galry

--- js/main.js
+++ js/main.js
@@ -137,8 +137,8 @@
 	};
 
 	// Set the date we're counting down to
-	var countDownDate = new Date("Dec 28, 2017 15:37:25").getTime();
-
+	var countDownDate = new Date("Dec 03, 2022 19:30:01").getTime()
+	
 	// Update the count down every 1 second
 	var x = setInterval(function() {
 
